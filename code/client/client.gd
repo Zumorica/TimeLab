76bfa3eb8a74f858579c57db01bef26a14eed56e
Zumo <apvictor00@hotmail.com>
2016-10-12 12:11:07 +0200

    Synces clients.

--- code/client/client.gd
+++ code/client/client.gd
@@ -4,32 +4,40 @@ var mob
 
 func _ready():
 	mob = get_node("Mob")
-	if get_node("/root/lobby").started_networking:
-		set_name(str(get_tree().get_network_unique_id()))
+	print("I LIVE. AGAIN.", get_name(), get_node("..").get_name())
 	set_process(true)
 	set_process_input(true)
 	
 func _process(dt):
 	pass
 
+sync func change_name(name):
+	set_name(name)
+
 sync func move_local_y(quantity, boolean):
 	mob.move_local_y(quantity, boolean)
+	rpc("update_global_pos", get_global_pos())
 	
 sync func move_local_x(quantity, boolean):
 	mob.move_local_x(quantity, boolean)
+	rpc("update_global_pos", get_global_pos())
+
+remote func update_global_pos(pos):
+	set_global_pos(pos)
 
 func _input(event):
-	if (event.is_action("ui_up")):
-		rpc("move_local_y", -32, true)
+	if get_name() == str(get_tree().get_network_unique_id()):
+		if (event.is_action("ui_up")):
+			rpc("move_local_y", -32, true)
+			
+		if (event.is_action("ui_down")):
+			rpc("move_local_y", 32, true)
 		
-	if (event.is_action("ui_down")):
-		rpc("move_local_y", 32, true)
-		
-	if (event.is_action("ui_left")):
-		rpc("move_local_x", -32, true)
-		
-	if (event.is_action("ui_right")):
-		rpc("move_local_x", 32, true)
+		if (event.is_action("ui_left")):
+			rpc("move_local_x", -32, true)
+			
+		if (event.is_action("ui_right")):
+			rpc("move_local_x", 32, true)
 
 func change_mob(mob):
 	pass
--- code/network/lobby.gd
+++ code/network/lobby.gd
@@ -7,8 +7,10 @@ var host
 var info = {name = "Generic Scientist", version = "0"}
 var player_list = {}
 var started_networking = false
+var client
 
 func _ready():
+	client = preload("res://scenes/client/Client.tscn")
 	host = NetworkedMultiplayerENet.new()
 	host.connect("connection_succeeded", self, "_connected_ok")
 	host.connect("connection_failed", self, "_connected_fail")
@@ -33,7 +35,9 @@ func join(ip, port):
 func prepare_map():
 	get_node("/root/Menu").queue_free()
 	var game = preload("res://scenes/game/game.tscn")
-	get_tree().get_root().add_child(game.instance())
+	game = game.instance()
+	game.get_node("Map/Client").set_name(str(get_tree().get_network_unique_id()))
+	get_tree().get_root().add_child(game)
 
 func close_connection():
 	get_tree().set_network_peer(null)
@@ -42,6 +46,10 @@ func close_connection():
 func _player_connected(id):
 	print("new memer", id)
 	rpc("register_player", get_tree().get_network_unique_id(), info)
+	if get_tree().get_network_unique_id() != id:
+		for client in player_list:
+			rpc_id(id, "register_player", client, player_list[client])
+
 
 func _player_disconnected(id):
 	pass
@@ -50,7 +58,10 @@ remote func register_player(id, info):
 	if not player_list.has(id):
 		player_list[id] = info
 		print("ohh new", id, "btw i am ", get_tree().get_network_unique_id())
-		
+		var map = get_node("/root/Game/Map")
+		var c = client.instance()
+		map.add_child(c)
+		c.rpc("change_name", str(id))
 	
 func _connected_ok():
 	print("Connected successfully!")
