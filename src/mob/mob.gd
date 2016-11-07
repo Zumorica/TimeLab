92fc6b86396cf3b0143256e84c5f7692b9784b8a
Zumo <apvictor00@hotmail.com>
2016-11-07 18:42:28 +0100

    Mobs can now have different sprites for each direction they're facing.

--- src/mob/mob.gd
+++ src/mob/mob.gd
@@ -2,10 +2,34 @@ extends KinematicBody2D
 
 signal _on_client_input(event)
 
+const NORTH = 0
+const SOUTH = 1
+const WEST = 2
+const EAST = 3
+
 export(String, "generic", "human") var race
+export(int, "NORTH", "SOUTH", "WEST", "EAST") var direction = 0
+export(Texture) var sprite_north
+export(Texture) var sprite_south
+export(Texture) var sprite_west
+export(Texture) var sprite_east
 
 func _ready():
 	pass
 
 func _on_client_input(event):
-	print(event)
+	pass
+
+func _on_Mob_draw():
+	if direction == NORTH:
+		rpc("_change_Sprite", sprite_north)
+	elif direction == SOUTH:
+		rpc("_change_Sprite", sprite_south)
+	elif direction == WEST:
+		rpc("_change_Sprite", sprite_west)
+	elif direction == EAST:
+		rpc("_change_Sprite", sprite_east)
+
+sync func _change_Sprite(texture):
+	if typeof(texture) == TYPE_IMAGE:
+		get_node("Sprite").set_texture(texture)
