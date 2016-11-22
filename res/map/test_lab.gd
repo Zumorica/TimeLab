d79ba1c0e139b287252833a5b911aa5ac948c852
Zumo <apvictor00@hotmail.com>
2016-11-22 19:43:24 +0100

    Update maps.
    +Entities can now be added to maps.
    +Merges lots of functions.

--- res/map/test_lab.gd
+++ res/map/test_lab.gd
@@ -2,4 +2,5 @@ extends "res://src/map/map.gd".Map
 
 func _init():
 	data = [{"type" : TILE, "ID" : BLUE_FLOOR, "x": 0, "y" : 0, "variables" : {}},
-			{"type" : TILE, "ID" : GREY_WALL, "x": 1, "y" : 0, "variables" : {}}]
+			{"type" : TILE, "ID" : GREY_WALL, "x": 1, "y" : 0, "variables" : {}},
+			{"type" : ENTITY, "ID" : SPAWN_POINT, "x": 0, "y" : 0, "variables" : {}}]
--- src/map/map.gd
+++ src/map/map.gd
@@ -5,16 +5,20 @@ class Map:
 	const OBJ = "obj"
 	const ITEM = "item"
 	const MOB = "mob"
+	const ENTITY = "entity"
 	
 	const TILE_BASE = preload("res://src/tile/tile.gd")
 	const OBJ_BASE = preload("res://src/obj/obj.gd")
 	const ITEM_BASE = preload("res://src/item/item.gd")
 	const MOB_BASE = preload("res://src/mob/mob.gd")
+	const ENTITY_BASE = preload("res://src/entity/entity.gd")
 	
 	const BLUE_FLOOR = 1
 	const GREY_WALL = 2
 	
 	const HUMAN_MOB = 1
+	
+	const SPAWN_POINT = 1
 	
 	const TILES = {1 : preload("res://src/tile/floor/blue.tscn"),
 				   2 : preload("res://src/tile/wall/grey.tscn")}
@@ -25,12 +29,15 @@ class Map:
 	
 	const ITEMS = {}
 	
+	const ENTITIES = {1 : preload("res://src/entity/spawn/spawn.tscn")}
+	
+	var size = false
 	var data = []
 	var _updated_data = []
 	
+	
 	func _ready():
 		set_name("Map")
-		create_map()
 		
 	func map_pos_to_px(vector, central = false):
 		if central:
@@ -55,113 +62,42 @@ class Map:
 			instance.set(variable, variables[variable])
 		return
 		
-	func _create_tile(tile):
-		# Do not call this function directly.
-		assert (typeof(tile) == TYPE_DICTIONARY)
-		assert (tile["type"] == TILE)
-		var instanced_tile = TILES[tile["ID"]].instance()
-		instanced_tile.set_pos(map_pos_to_px(Vector2(tile["x"], tile["y"]), true))
-		_load_instance_variables(tile["variables"], instanced_tile)
-		return instanced_tile
-		
-	func _create_object(obj):
-		# Do not call this function directly.
-		assert (typeof(obj) == TYPE_DICTIONARY)
-		assert (obj["type"] == OBJ)
-		var instanced_obj = OBJS[obj["ID"]].instance()
-		instanced_obj.set_pos(map_pos_to_px(Vector2(obj["x"], obj["y"]), true))
-		_load_instance_variables(obj["variables"], instanced_obj)
-		return instanced_obj
-		
-	func _create_item(item):
-		# Do not call this function directly.
-		assert (typeof(item) == TYPE_DICTIONARY)
-		assert (item["type"] == ITEM)
-		var instanced_item = ITEMS[item["ID"]].instance()
-		instanced_item.set_pos(map_pos_to_px(Vector2(item["x"], item["y"]), true))
-		_load_instance_variables(item["variables"], instanced_item)
-		return instanced_item
-		
-	func _create_mob(mob):
-		# Do not call this function directly.
-		assert (typeof(mob) == TYPE_DICTIONARY)
-		assert (mob["type"] == MOB)
-		var instanced_mob = MOBS[mob["ID"]].instance()
-		instanced_mob.set_pos(map_pos_to_px(Vector2(mob["x"], mob["y"]), true))
-		_load_instance_variables(mob["variables"], instanced_mob)
-		return instanced_mob
-		
 	func create_instance_from_data_entry(data):
+		assert (typeof(data) == TYPE_DICTIONARY)
+		var dict
 		if data["type"] == TILE:
-			return _create_tile(data)
+			dict = TILES
 		elif data["type"] == OBJ:
-			return _create_object(data)
+			dict = OBJS
 		elif data["type"] == ITEM:
-			return _create_item(data)
+			dict = ITEMS
 		elif data["type"] == MOB:
-			return _create_mob(data)
+			dict = MOBS
+		elif data["type"] == ENTITY:
+			dict = ENTITIES
 		else:
 			return
+		var instanced = dict[data["ID"]].instance()
+		instanced.set_pos(map_pos_to_px(Vector2(data["x"], data["y"]), true))
+		_load_instance_variables(data["variables"], instanced)
+		return instanced
 		
-	func _process_tile(instanced_tile):
-		# Do not call this function directly.
-		assert (typeof(instanced_tile) == TYPE_OBJECT)
-		assert (instanced_tile extends TILE_BASE)
-		var tile_data = {"type" : TILE}
-		var map_pos = px_pos_to_map(instanced_tile.get_pos())
-		tile_data["ID"] = instanced_tile.get_ID()
-		tile_data["x"] = map_pos.x
-		tile_data["y"] = map_pos.y
-		tile_data["variables"] = _save_instance_variables(instanced_tile)
-		return tile_data
-		
-	func _process_obj(instanced_obj):
-		# Do not call this function directly.
-		assert (typeof(instanced_obj) == TYPE_OBJECT)
-		assert (instanced_obj extends OBJ_BASE)
-		var obj_data = {"type" : OBJ}
-		var map_pos = px_pos_to_map(instanced_obj.get_pos())
-		obj_data["ID"] = instanced_obj.get_ID()
-		obj_data["x"] = map_pos.x
-		obj_data["y"] = map_pos.y
-		obj_data["variables"] = _save_instance_variables(instanced_obj)
-		return obj_data
-		
-		
-	func _process_item(instanced_item):
-		# Do not call this function directly.
-		assert (typeof(instanced_item) == TYPE_OBJECT)
-		assert (instanced_item extends ITEM_BASE)
-		var item_data = {"type" : ITEM}
-		var map_pos = px_pos_to_map(instanced_item.get_pos())
-		item_data["ID"] = instanced_item.get_ID()
-		item_data["x"] = map_pos.x
-		item_data["y"] = map_pos.y
-		item_data["variables"] = _save_instance_variables(instanced_item)
-		return item_data
-		
-		
-	func _process_mob(instanced_mob):
-		# Do not call this function directly.
-		assert (typeof(instanced_mob) == TYPE_OBJECT)
-		assert (instanced_mob extends MOB_BASE)
-		var mob_data = {"type" : MOB}
-		var map_pos = px_pos_to_map(instanced_mob.get_pos())
-		mob_data["ID"] = instanced_mob.get_ID()
-		mob_data["x"] = map_pos.x
-		mob_data["y"] = map_pos.y
-		mob_data["variables"] = _save_instance_variables(instanced_mob)
-		return mob_data
-		
-	func create_data_from_instance(instance):
-		if instance extends TILE_BASE:
-			pass
-		elif instance extends OBJ_BASE:
-			pass
-		elif instance extends ITEM_BASE:
-			pass
-		elif instance extends MOB_BASE:
-			pass
+	func create_data_from_instance(instanced):
+		assert (typeof(instanced) == TYPE_OBJECT)
+		var type
+		if instanced extends TILE_BASE:
+			type = TILE
+		elif instanced extends OBJ_BASE:
+			type = OBJ
+		elif instanced extends ITEM_BASE:
+			type = ITEM
+		elif instanced extends MOB_BASE:
+			type = MOB
+		elif instanced extends ENTITY_BASE:
+			type = ENTITY
+		var map_pos = px_pos_to_map(instanced.get_pos())
+		var data = {"type" : type, "ID" : instanced.get_ID(), "x" : map_pos.x, "y" : map_pos.y, "variables" : _save_instance_variables(instanced)}
+		return data
 		
 	func create_map(original = true):
 		for child in get_children():
@@ -170,13 +106,14 @@ class Map:
 			for tile in data:
 				add_child(create_instance_from_data_entry(tile))
 		else:
+			update_data()
 			for tile in _updated_data:
 				add_child(create_instance_from_data_entry(tile))
 
 	func update_data():
 		_updated_data = []
 		for child in get_children():
-			_updated_data.append(_process_children(child))
+			_updated_data.append(create_data_from_instance(child))
 		return _updated_data
 		
 	func get_data(original = false):
