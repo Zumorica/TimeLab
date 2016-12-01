6fc8eff750497e35374b143cd24bff5887b43654
Zumo <apvictor00@hotmail.com>
2016-12-01 16:59:33 +0100

    Makes editor work with the new map format.

--- src/map/editor/editor_map.gd
+++ src/map/editor/editor_map.gd
@@ -4,12 +4,11 @@ var draw_grid = true
 var mouse_pressed = false
 
 func _ready():
-	set_process_input(true)
-	map_size = Vector2(24, 24)
+	initial_map_preparation()
 
 func _draw():
 	if draw_grid:
-		var map_ssize = map_pos_to_px(map_size)
+		var map_ssize = map_pos_to_px(get_map_size())
 		for x in range(0, map_ssize.x + 1):
 			if not (x % 32):
 				draw_line(Vector2(x, 0), Vector2(x, map_ssize.y), Color(255, 255, 255, 255))
@@ -18,6 +17,7 @@ func _draw():
 				draw_line(Vector2(0, y), Vector2(map_ssize.x, y), Color(255, 255, 255, 255))
 
 func _input(event):
+	var map_size = get_map_size()
 	var camera = get_node("Camera2D")
 	if event.is_action_pressed("map_editor_zoom_up") and camera.get_zoom().x <= 4:
 		camera.set_zoom(camera.get_zoom() + Vector2(0.1, 0.1))
@@ -33,12 +33,10 @@ func _input(event):
 			if get_parent().cursor_mode == 2:
 				var data = get_parent().current_data
 				if data["type"] != null and data["ID"] != null:
-					if not check_entry_in_data(mpos_map, false, data["type"], false, true).size():
-						data["x"] = mpos_map.x
-						data["y"] = mpos_map.y
+					if not find(mpos_map, false, data["type"]).size():
+						data["position"] = Vector3(mpos_map.x, mpos_map.y, get_current_floor())
 						# Append child to updated data and then create map maybe?
 						add_child_from_data(data)
-						create_map(false)
 
 	if event.is_action_released("map_editor_mouse_left"):
 		mouse_pressed = false
@@ -51,3 +49,7 @@ func _on_Grid_CheckBox_toggled( pressed ):
 	print(pressed)
 	print(draw_grid)
 	update()
+
+func _on_SpinBox_value_changed( value ):
+	set_current_floor(value)
+	get_node("../GUI/Selected/SpinBox").set_max(get_map_size().z)
