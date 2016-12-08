6b67d486f326d2d70762891b5513281d802f4695
Zumo <apvictor00@hotmail.com>
2016-12-08 14:19:48 +0100

    Updated element.
    Removed dense.
    Elements can now extend from anything that extends PhysicsBody2D.

--- src/element/element.gd
+++ src/element/element.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends PhysicsBody2D
 
 signal collision(other)
 signal collided(other)
@@ -13,21 +13,13 @@ const direction_index = {0 : Vector2(0, -32),
 						 3 : Vector2(32, 0)}
 						
 export(int, "NORTH", "SOUTH", "WEST", "EAST") var direction = 0
-export(bool) var dense = false
 var z_floor = 0 setget set_floor,get_floor
 var last_pos = Vector2(0, 0)
 var last_move = Vector2(0, 0)
-var noclip = false
 
 func set_floor(z):
 		z_floor = z
-	
-func set_dense(boolean):
-	dense = boolean
-	
-func is_dense():
-	return dense
-	
+
 func get_floor():
 	return z_floor
 	
@@ -40,21 +32,6 @@ func set_pos3(vector):
 func get_pos3():
 	var pos = get_pos()
 	return Vector3(pos.x, pos.y, get_floor())
-	
-func move_to(direction):
-	# for moving in directions
-	assert typeof(direction) == TYPE_INT and direction in range(0, 4)
-	
-func move_by(vector):
-	# for moving by vectors
-	assert typeof(vector) == TYPE_VECTOR2
-	
-func can_move(where):
-	if typeof(where) == TYPE_VECTOR2:
-		pass
-	elif typeof(where) == TYPE_INT and where in range(0, 4):
-		pass
-	return false
 
 func _ready():
 	set_pickable(true)
