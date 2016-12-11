df9c683e3f319fd383bb458e3251cc426c81c68a
Zumo <apvictor00@hotmail.com>
2016-12-11 17:20:22 +0100

    Elements v2 oh boy
    -Added attacking.
    -Added intents.
    -Added bitflags.
    -Etc.

--- src/element/element.gd
+++ src/element/element.gd
@@ -4,7 +4,21 @@ signal on_direction_change(direction)
 signal on_collided(collider)	# When something collides with the element.
 signal on_collide(collider)		# When the element collides with something.
 signal on_clicked()
+signal on_interacted(item)
+signal on_damaged(damage, other)
+signal on_attack(other)
+signal on_death()
 
+# State bit flags
+const DEAD = int(pow(2,0))
+const BURNING = int(pow(2,1))
+const MUTE = int(pow(2,2))
+const BLIND = int(pow(2,3))
+const DEAF = int(pow(2,4))
+const CANT_WALK = int(pow(2,5))
+const CANT_ATTACK = int(pow(2,6))
+
+# Direction constants
 const NORTH = 0
 const SOUTH = 1
 const WEST = 2
@@ -13,13 +27,29 @@ const direction_index = {0 : Vector2(0, -1),
 						 1 : Vector2(0, 1),
 						 2 : Vector2(-1, 0),
 						 3 : Vector2(1, 0)}
-						
+
+# Intents.
+
+const INTENT_NONE = 0
+const INTENT_INTERACT = 1
+const INTENT_ATTACK = 2
+
 export(int, "NORTH", "SOUTH", "WEST", "EAST") var direction = 0
-var z_floor = 0 setget set_floor,get_floor
+export(int, "No intent", "Interact intent", "Attack intent") var intent = 1 setget set_intent, get_intent
+export(int) var max_health = 100
+export(bool) var invincible = false
+export(float) var damage_factor = 1.0
+export(float) var burn_damage_factor = 1.0
+export(float) var attack_factor = 1.0
+export(float) var attack_delay = 0.5
+export(int, FLAGS) var state = 0
+export(int) var speed = 80
+export(int) var interact_range = 100
+var z_floor = 0 setget set_floor,get_floor # Might get removed in the future.
 var last_pos = Vector2(0, 0)
 var last_move = Vector2(0, 0)
 var last_collider = null
-var speed = 128
+var health = max_health
 
 func set_floor(z):
 		z_floor = z
@@ -37,6 +67,42 @@ func get_pos3():
 	var pos = get_pos()
 	return Vector3(pos.x, pos.y, get_floor())
 
+func get_intent():
+	return intent
+	
+func set_intent(new_intent):
+	if new_intent != INTENT_NONE or new_intent != INTENT_INTERACT or new_intent != INTENT_ATTACK:
+		return
+	else:
+		intent = new_intent
+
+func reset_attack_timer():
+	if ((state & CANT_ATTACK) == CANT_ATTACK):
+		state ^= CANT_ATTACK
+
+sync func damage(damage, other):
+	if (not invincible):
+		health -= round(damage * damage_factor)
+		emit_signal("on_damaged", damage, other)
+		if (health <= 0):
+			health = 0
+			state |= DEAD
+			emit_signal("on_death")
+
+func attack(other, bonus = 0):
+	if (not (state & DEAD) and not (state & CANT_ATTACK)) and other extends get_node("/root/timeline").element_base:
+		var damage = (randi()%11) * (attack_factor + bonus)
+		other.rpc("damage", damage, self)
+		rpc("emit_on_attack", other)
+		state |= CANT_ATTACK
+		get_node("AttackTimer").start()
+
+sync func emit_on_attack(other):
+	emit_signal("on_attack", other)
+
+sync func emit_on_interacted(item):
+	emit_signal("on_interacted", item)
+
 slave func _update_pos(pos):
 	set_pos(pos)
 
@@ -44,11 +110,35 @@ slave func _update_direction(direction):
 	direction = direction
 	emit_signal("on_direction_change", direction)
 
+func _on_clicked():
+	var cmob = get_node("/root/timeline").own_client.get_mob()
+	if cmob:
+		var intention = cmob.get_intent()
+		if intention  == INTENT_INTERACT:
+			rpc("emit_on_interacted", false)
+		elif intention == INTENT_ATTACK:
+			cmob.attack(self)
+			
 func _ready():
 	set_pickable(true)
 	set_fixed_process(true)
 	set_process_input(true)
-	
+	var attack_timer = Timer.new()
+	attack_timer.set_wait_time(attack_delay)
+	attack_timer.connect("timeout", self, "reset_attack_timer")
+	attack_timer.set_name("AttackTimer")
+	attack_timer.set_one_shot(true)
+	add_child(attack_timer)
+	if not is_connected("on_clicked", self, "_on_clicked"):
+		connect("on_clicked", self, "_on_clicked")
+	if not is_connected("on_attack", self, "_on_attack"):
+		connect("on_attack", self, "_on_attack")
+	if not is_connected("input_event", self, "_input_event"):
+			connect("input_event", self, "_input_event")
+
+func _on_attack(other):
+	pass
+
 func _fixed_process(dt):
 	if get_node("/root/timeline").is_online:
 		if self extends KinematicBody2D:
@@ -89,6 +179,7 @@ func _fixed_process(dt):
 					rpc_unreliable("_update_direction", direction)
 					emit_signal("on_direction_change", direction)
 			
-func _input(event):
-	if event.is_action("left_click") and not event.is_echo():
+func _input_event(viewport, event, shape):
+	if event.is_action_pressed("left_click") and not event.is_echo():
 		emit_signal("on_clicked")
+		get_tree().set_input_as_handled()
