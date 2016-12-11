cd1b613b85877ad26dcefa0d8b955c58f09473f7
Zumo <apvictor00@hotmail.com>
2016-12-11 19:41:28 +0100

    Doors now close automatically.

--- src/obj/machine/door/door.gd
+++ src/obj/machine/door/door.gd
@@ -6,10 +6,17 @@ const OPENING = 2
 const CLOSING = 3
 
 var door_state = CLOSED
+export(float) var close_delay = 5
 	
 func _ready():
 	set_pickable(true)
 	set_process_input(true)
+	var close_timer = Timer.new()
+	close_timer.set_name("CloseTimer")
+	close_timer.set_wait_time(close_delay)
+	close_timer.set_one_shot(true)
+	close_timer.connect("timeout", self, "close")
+	add_child(close_timer)
 	if not is_connected("on_collided", self, "_on_collided"):
 		connect("on_collided", self, "_on_collided")
 	if not is_connected("on_interacted", self, "_on_interacted"):
@@ -17,6 +24,7 @@ func _ready():
 	
 sync func open():
 	get_node("AnimationPlayer").play("open_animation")
+	get_node("CloseTimer").start()
 	
 sync func close():
 	get_node("AnimationPlayer").play("close_animation")
