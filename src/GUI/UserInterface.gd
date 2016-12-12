0f1811290c6f32c4cf8aa9379f85d43fd3210596
ZackUnfair <acezackfair@gmail.com>
2016-12-12 20:01:01 +0100

    Fixed resizing.

--- src/GUI/UserInterface.gd
+++ src/GUI/UserInterface.gd
@@ -2,20 +2,19 @@ extends Control
 
 var is_chat_visible = false
 var timer
-onready var chat_window = get_node("Layer/Container/Chat/ChatWindow")
-onready var text_input = get_node("Layer/Container/Chat/TextInput")
+onready var chat_window = get_node("Layer/Chat/ChatWindow")
+onready var text_input = get_node("Layer/Chat/TextInput")
 
 func _ready():
-	var container = get_node("Layer/Container")
-	get_node("Layer/Container/FPSCount").set_pos(Vector2(0, 0))
+	get_node("Layer/FPSCount").set_pos(Vector2(0, 0))
 	set_process(true)
 	set_process_input(true)
-	timer = get_node("Layer/Container/Chat/Timer")
+	timer = get_node("Layer/Chat/Timer")
 	timer.connect("timeout", self, "close_chat")
 	timer.set_one_shot(true)
 
 func _process(dt):
-	get_node("Layer/Container/FPSCount").set_text(str(OS.get_frames_per_second()))
+	get_node("Layer/FPSCount").set_text(str(OS.get_frames_per_second()))
 
 func _input(ev):
 	if ev.type == InputEvent.MOUSE_MOTION:
@@ -50,7 +49,7 @@ func send_message():
 	rpc("_update_chat", msg)
 
 sync func _update_chat(msg):
-	var messages = get_node("Layer/Container/Chat/ChatWindow/Messages")
+	var messages = get_node("Layer/Chat/ChatWindow/Messages")
 	messages.get_parent().show()
 	messages.append_bbcode(msg)
 	messages.newline()
