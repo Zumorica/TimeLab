e31ef9c8b55539153fa5b03bad718ad6ad19cd26
Zumo <apvictor00@hotmail.com>
2016-12-18 14:24:26 +0100

    Fixed typo in code.

--- src/obj/machine/door/door.gd
+++ src/obj/machine/door/door.gd
@@ -34,7 +34,7 @@ func _on_collided(other):
 		rpc("open")
 
 func _on_interacted(other, item):
-	if ther.get_pos().distance_to(get_pos()) < 100 and other.get_intent() == INTENT_INTERACT:
+	if other.get_pos().distance_to(get_pos()) < 100 and other.get_intent() == INTENT_INTERACT:
 		if door_state != OPENING or door_state != CLOSING:
 			if door_state == OPEN:
 				rpc("close")
