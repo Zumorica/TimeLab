a314586e64067de95e8d1a5e2d54ba04002fddf4
ZackUnfair <acezackfair@gmail.com>
2016-12-29 21:13:05 +0100

    Some progress with container.gd

--- src/container/container.gd
+++ src/container/container.gd
@@ -6,21 +6,36 @@ export(int) var storage_size = 10
 export(NodePath) var display_node
 
 var storage = {}
-# Containers will override the slot_list with their own slot (names or nodes though?)
+# Contains the slot nodes
 var slot_list = []
 
+func _ready():
+	display_node.hide()
+	for c in display_node.get_children():
+		slot_list.append(c)
+
+func is_full():
+	return storage.size() == storage_size
+
 func store_item(item, slot=null):
+	if is_full():
+		return
 	if !slot:
 		# If no slot is provided, by default the item will be stored in the first empty slot
-		for s in slot_list:
-			if not storage.has(s):
+		for s in display_node.get_children():
+			if not s.has_item():
 				slot = s
 				break
 	
 	storage[slot] = item
+	slot.add_child(item)
 
 func remove_item(item):
-	pass
+	for k in storage:
+		if storage[k] == item:
+			k.remove_child(item)
+			storage.erase(k)
+			break
 
 func display():
-	pass
+	display_node.show()
