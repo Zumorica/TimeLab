2516ee52b4433c8a18fb9845b132116b83efbb79
ZackUnfair <acezackfair@gmail.com>
2017-01-03 00:50:11 +0100

    Picking up an item now adds it to your hand.

--- src/item/item.gd
+++ src/item/item.gd
@@ -13,7 +13,7 @@ func _ready():
 func _on_interacted(other, item):
 	var client = other.get_client()
 	var inv = client.get_node("UserInterface/Layer/Inventory")
-	inv.add_item(self)
+	inv.add_item(self, inv.get_node("Background/RHandSlot"))
 
 func get_inventory_texture():
 	return inventory_image
