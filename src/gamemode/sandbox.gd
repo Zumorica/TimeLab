14d6daa6961da39e27dd2203672602947cdd6b22
Zumo <apvictor00@hotmail.com>
2017-01-11 11:44:09 +0100

    Cherrypick part 6: Branch ocean

---
+++ src/gamemode/sandbox.gd
@@ -0,0 +1,5 @@
+extends "res://src/gamemode/gamemode.gd"
+
+func _init():
+	name = "Sandbox mode"
+
--- src/menu/gamemodeSelection.gd
+++ src/menu/gamemodeSelection.gd
@@ -2,3 +2,5 @@ extends OptionButton
 
 func _ready():
 	hide()
+	for key in timeline.gamemode_list.keys():
+		add_item(key, timeline.gamemode_list.keys().find(key))
