1c5a9d13ef7ae7f68d99adfd17dbc04deafbb184
Zumo <zddm@outlook.es>
2017-01-27 13:52:10 +0100

    Add "plural gender". Adds way to get pronouns/possesives.

--- src/globals/gender.gd
+++ src/globals/gender.gd
@@ -3,3 +3,7 @@ extends Node
 const NEUTRAL = 0
 const MALE = 1
 const FEMALE = 2
+const PLURAL = 3
+
+const possesive = {NEUTRAL : "its", MALE : "his", FEMALE : "her", PLURAL : "their"}
+const pronoun = {NEUTRAL : "it", MALE : "he", FEMALE : "she", PLURAL : "they"}
