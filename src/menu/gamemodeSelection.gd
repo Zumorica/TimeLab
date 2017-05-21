9703eaed25e0a0f6425e846b10bd4e24f73f7898
Zumo <zddm@outlook.es>
2017-05-21 15:29:44 +0200

    Adds a single space to a line of code.

--- src/menu/gamemodeSelection.gd
+++ src/menu/gamemodeSelection.gd
@@ -2,5 +2,5 @@ extends OptionButton
 
 func _ready():
 	hide()
-	for key intimelab.timeline.gamemode_list.keys():
+	for key in timelab.timeline.gamemode_list.keys():
 		add_item(key,timelab.timeline.gamemode_list.keys().find(key))
