3dfe3f85d1ed71036c224b42f92f3af5a10764ec
Zumo <zddm@outlook.es>
2017-05-21 13:00:33 +0200

    Something something I'm lazy

---
+++ src/element/element.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+var state = 0
+export var is_solid = false  # Whether you can walk through this element or not.
+export var is_opaque = false # Whether you can see through this element or not.
+
+func _ready():
+	pass
