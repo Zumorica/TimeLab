e55bb834e3115063377397ac8985499c21ca5e10
Zumo <zddm@outlook.es>
2017-05-27 19:16:38 +0200

    Title menu now works. Now you can join games or host servers.

---
+++ src/menu/title/title.gd
@@ -0,0 +1,37 @@
+extends CenterContainer
+
+export(NodePath) var join_port
+export(NodePath) var join_address
+export(NodePath) var join_button
+export(NodePath) var host_port
+export(NodePath) var host_maxplayers
+export(NodePath) var host_button
+
+func _ready():
+	assert get_node(join_port) extends LineEdit
+	assert get_node(join_address) extends LineEdit
+	assert get_node(join_button) extends Button
+	assert get_node(host_port) extends LineEdit
+	assert get_node(host_maxplayers) extends LineEdit
+	assert get_node(host_button) extends Button
+	get_node(join_button).connect("pressed", self, "_on_join_button_pressed")
+	get_node(host_button).connect("pressed", self, "_on_host_button_pressed")
+	
+func _on_join_button_pressed():
+	var port_node = get_node(join_port)
+	var address_node = get_node(join_address)
+	if not (port_node.text.is_valid_integer() and address_node.text.is_valid_ip_address()):
+		prints("Port", port_node.text.is_valid_integer(), "Address" , address_node.text.is_valid_ip_address())
+		return
+	var port = int(port_node.text)
+	var address = address_node.text
+	timelab.join_game(address, port)
+	
+func _on_host_button_pressed():
+	var port_node = get_node(host_port)
+	var maxplayers_node = get_node(host_maxplayers)
+	if not (port_node.text.is_valid_integer() and maxplayers_node.text.is_valid_integer()):
+		return
+	var port = int(port_node.text)
+	var maxplayers = int(maxplayers_node.text)
+	timelab.create_server(port, maxplayers)
