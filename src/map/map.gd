419c3dd5de18d10d94b1b58a24a9ebe4e7e26a46
Zumo <zddm@outlook.es>
2017-05-28 16:16:12 +0200

    Fixes loop
    GODOT, YOU FUCK. it was set to remote, not sync!

--- src/map/map.gd
+++ src/map/map.gd
@@ -8,7 +8,6 @@ func _ready():
 	else:
 		rpc_config("_on_tracked_element_move", RPC_MODE_SLAVE)
 	_grid_populate(get_used_rect().size)
-	timelab.set_current_map(self)
 
 func _grid_populate(grid_size):
 	# Clears the whole grid and populates it with
@@ -25,15 +24,17 @@ func track_element_on_map(element):
 		element.connect("move", self, "_on_tracked_element_move")
 		_on_tracked_element_move(element, element.cell_position, Vector2(0, 0))
 
-remote func _on_tracked_element_move(element, new_position, old_position):
+func _on_tracked_element_move(element, new_position, old_position):
+	if get_tree().get_network_unique_id() == 1:
+		rpc("_move_element_on_grid", element, new_position, old_position)
+
+sync func _move_element_on_grid(element, new_position, old_position):
 	assert grid.has_all([new_position, old_position])
 	if grid[old_position].has(element):
 		grid[old_position].erase(element)
 	if not grid[new_position].has(element):
 		grid[new_position].append(element)
-	if get_tree().get_network_unique_id() == 1:
-		rpc("_on_tracked_element_move", element, new_position, old_position)
-		
+
 sync func tracked_element_remove(element):
 	if element.is_connected("move", self, "on_tracked_element_move"):
 		element.disconnect("move", self, "on_tracked_element_move")
