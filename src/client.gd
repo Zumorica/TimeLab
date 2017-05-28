ba7673245f649970aedb85ce9b6260d40dcd992f
Zumo <zddm@outlook.es>
2017-05-28 16:14:45 +0200

    Weak ref was a bad idea. Also forgot to rename some functions.
    I'm totally not lazy

--- src/client.gd
+++ src/client.gd
@@ -5,7 +5,7 @@ signal mind_exit(mind)
 
 sync var ID
 sync var is_alive = true
-sync var _mind setget get_mind, set_mind # Weak ref.
+sync var _mind = null setget get_mind, set_mind
 sync var preround_ready = false
 
 func _ready():
@@ -16,25 +16,31 @@ func _ready():
 		set_network_mode(NETWORK_MODE_SLAVE)
 	add_to_group("clients")
 	
-func has_client():
-	return typeof(_mind.get_ref()) == TYPE_OBJECT
+func has_mind():
+	return (typeof(_mind) == TYPE_OBJECT)
 	
-func get_client():
-	return _mind.get_ref()
+func get_mind():
+	return _mind
 	
-func set_client(mind):
+func set_mind(mind):
 	if get_tree().is_network_server() or get_tree().get_network_unique_id() == ID:
 		assert typeof(mind) == TYPE_OBJECT
 		assert mind extends load(timelab.base.mind)
-		rset("_mind", weakref(mind))
-		if mind.get_client() != self:
+		rset("_mind", mind)
+		_mind = mind
+		print(_mind)
+		if mind.has_client():
+			if mind.get_client() != self:
+				mind.set_client(self)
+		else:
 			mind.set_client(self)
 		emit_signal("mind_enter", mind)
 			
-func remove_client():
-	if get_tree().is_network_server() or get_tree().get_network_unique_id() == client.ID:
-		var mind = get_mind()
-		rset("_mind", null)
-		if mind.get_client() == self:
-			mind.remove_mind()
-		emit_signal("mind_exit", mind)
+func remove_mind():
+	if has_mind():
+		if get_tree().is_network_server() or get_tree().get_network_unique_id() == ID:
+			var mind = get_mind()
+			rset("_mind", null)
+			if mind.get_client() == self:
+				mind.remove_mind()
+			emit_signal("mind_exit", mind)
--- src/module/mind/mind.gd
+++ src/module/mind/mind.gd
@@ -3,7 +3,7 @@ extends Camera2D
 signal client_enter(client)
 signal client_exit(client)
 
-sync var _client = null setget get_client, set_client # Weak ref.
+sync var _client = null setget get_client, set_client
 
 func _ready():
 	assert get_parent() extends load(timelab.base.element)
@@ -11,24 +11,49 @@ func _ready():
 	set_network_mode(NETWORK_MODE_INHERIT)
 	
 func has_client():
-	return typeof(_client.get_ref()) == TYPE_OBJECT
+	return (typeof(_client) == TYPE_OBJECT)
 	
 func get_client():
-	return _client.get_ref()
+	return _client
 	
 func set_client(client):
 	if get_tree().is_network_server() or get_tree().get_network_unique_id() == client.ID:
 		assert typeof(client) == TYPE_OBJECT
 		assert client extends load(timelab.base.client)
-		rset("_client", weakref(client))
-		if client.get_mind() != self:
+		rset("_client", client)
+		_client = client
+		if client.has_mind():
+			if client.get_mind() != self:
+				client.set_mind(self)
+		else:
 			client.set_mind(self)
+		if get_tree().get_network_unique_id():
+			get_parent().set_network_mode(NETWORK_MODE_MASTER)
+		else:
+			get_parent().rpc_id(client.ID, "set_network_mode", NETWORK_MODE_MASTER)
 		emit_signal("client_enter", client)
 
 func remove_client():
-	if get_tree().is_network_server() or get_tree().get_network_unique_id() == client.ID:
-		var client = get_client()
-		rset("_client", null)
-		if client.get_mind() == self:
-			client.remove_mind()
-		emit("client_exit", client)
+	if has_client():
+		if get_tree().is_network_server() or get_tree().get_network_unique_id() == get_client().ID:
+			var client = get_client()
+			rset("_client", null)
+			if client.get_mind() == self:
+				client.remove_mind()
+			emit("client_exit", client)
+		
+func _fixed_process(dt):
+	if has_client():
+		if get_tree().get_network_unique_id() == get_client().ID:
+			var movement = Vector2(0, 0)
+			if Input.is_action_pressed("move_left"):
+				movement += timelab.direction.WEST
+			if Input.is_action_pressed("move_right"):
+				movement += timelab.direction.EAST
+			if Input.is_action_pressed("move_up"):
+				movement += timelab.direction.NORTH
+			if Input.is_action_pressed("move_down"):
+				movement += timelab.direction.SOUTH
+			print(movement)
+			if movement.length() > 0:
+				print(get_parent().cell_slide(movement, true))
